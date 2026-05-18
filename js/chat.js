a445761f8f03d452bc3c121923a89c6d27712253
CesarIzk <125629498+CesarIzk@users.noreply.github.com>
2026-05-18 14:51:06 -0600

    update url

--- js/chat.js
+++ js/chat.js
@@ -1,5 +1,5 @@
 // 🔧 Cambia esta URL por la de tu backend en Railway
-const API_URL = "https://tu-app.up.railway.app";
+const API_URL = "https://poi-back-production.up.railway.app";
 
 const socket = io(API_URL);
 
