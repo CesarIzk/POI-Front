2c3d3c9c913ab980372149552475b05b0a31f99b
CesarIzk <125629498+CesarIzk@users.noreply.github.com>
2026-05-19 17:36:55 -0600

    change url

--- js/login.js
+++ js/login.js
@@ -1,5 +1,5 @@
 // 🔧 Cambia esta URL por la de tu backend en Railway
-const API_URL = "https://poi-back-production.up.railway.app";
+const API_URL = "https://overbitterly-convuluted-katharina.ngrok-fr-";
 
 document.getElementById("loginForm").addEventListener("submit", async (e) => {
     e.preventDefault();
