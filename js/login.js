9f08ecc7ea05385f3c7d2d816fcf946002f511a7
GabrielaZarazua <irma.zarazuahz@uanl.edu.mx>
2026-05-24 21:25:58 -0600

    fix url

--- js/login.js
+++ js/login.js
@@ -1,5 +1,5 @@
 // 🔧 Cambia esta URL por la de tu backend en Railway
-const API_URL = "https://poi-back-production.up.railway.app";
+const API_URL = "poi-back-production-7931.up.railway.app";
 
 document.getElementById("loginForm").addEventListener("submit", async (e) => {
     e.preventDefault();
